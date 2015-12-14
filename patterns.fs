1242ca5cfdf7877e43f87098fe54b61a26a59cfe
lucapiccinelli <luca.picci@gmail.com>
2015-12-14 07:09:07 +0100

    completato tutorial sugli active pattern

--- patterns.fs
+++ patterns.fs
@@ -27,17 +27,20 @@ match v with
 | Vector -> printfn "vector1"
 
 
-let rec isPalindrome (s:string) (from:int) (to:int) = 
+let rec isPalindrome (s:string) (fromIdx:int) (toIdx:int) = 
     if s = null then false
-    elif to - from < 2 then true
-    elif s.[from] = s.[to - 1] then
-        isPalindrome s (from + 1) (to - 1)
+    elif toIdx - fromIdx < 2 then true
+    elif s.[fromIdx] = s.[(toIdx - 1)] then
+        isPalindrome s (fromIdx + 1) (toIdx - 1)
     else false
  
 let (|PALINDROME|_|) (s:string) = 
     if isPalindrome s 0 s.Length then Some s
     else None
 
-match "aba" with
-| PALINDROME(v) -> printfn "%s palidrome" v
-| _ -> "No pal"  
+let sayPalindrome s = 
+    match s with
+    | PALINDROME(v) -> printfn "%s palidrome" v
+    | _ -> printfn "No pal"
+
+sayPalindrome "aba"  
